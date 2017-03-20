9ab0733b480efe6e2ab83ba17a99db57aef51e59
apurva <akarve@thoughtworks.com>
2017-03-20 14:10:53 +0530

    Adding functionality for checking if the number is even

---
+++ src/EvenOddNumber.java
@@ -0,0 +1,12 @@
+public class EvenOddNumber {
+    private final int number;
+
+    public EvenOddNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isEven() {
+        return true;
+    }
+
+}
---
+++ test/EvenOddTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EvenOddTest {
+    @Test
+    public void isEvenShouldReturnTrueForNumber2() throws Exception {
+        EvenOddNumber evenOdd = new EvenOddNumber(2);
+        assertTrue(evenOdd.isEven());
+    }
+
+    @Test
+    public void isEvenShouldReturnFalseForNumber3() throws Exception {
+        EvenOddNumber evenOdd = new EvenOddNumber(3);
+        assertFalse(evenOdd.isEven());
+    }
+    
+}
